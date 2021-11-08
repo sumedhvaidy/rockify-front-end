5b423d57220b92415a3063f70e1c4db64a8af000
CSryanf3 <ryanf3@illinois.edu>
2021-11-08 12:35:04 -0600

    Basic UI done, button functionality needed

--- rockify-front-end/src/App.js
+++ rockify-front-end/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from "react";
+import Axios from 'axios';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className = "searchbar">
+        <h1> Rockify Demo </h1>
+        <p> Insert an artist into the database</p>
+        <label> Artist: </label>
+        <input type="text" name = "InsertArtist"/>
+        <button> Insert </button>
+        <p> Delete an artist from the database</p>
+        <label> Artist: </label>
+        <input type="text" name = "DeleteArtist"/>
+        <button> Delete </button>
+        <p>Search for Albums</p>
+        <label> Artist: </label>
+        <input type="text" name = "Artist"/>
+        <br></br>
+        <label> Year [From - To]: </label>
+        <input type="number" name = "DateStart"/>
+        <input type="number" name = "DateEnd"/>
+        <br></br>
+        <label> Popularity Rating [0 - 100] </label>
+        <input type="number" name = "Popularity"/>
+        <br></br>
+        <label> Duration [in ms] </label>
+        <input type="number" name = "DurationStart"/>
+        <input type="number" name = "DurationEnd"/>
+        <br></br>
+        <button> Search </button>
+
+      </div>
     </div>
   );
 }
