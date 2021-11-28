948a393b978ab058cd2df7b046b77faa009856c8
CSryanf3 <ryanf3@illinois.edu>
2021-11-27 20:41:45 -0600

    Basic UI for login, search, and recommend pages

--- rockify-front-end/src/App.js
+++ rockify-front-end/src/App.js
@@ -12,32 +12,71 @@ const App = () => (
 
 const Navigation = () => (
     <div class = "menu">
-      <li><NavLink activeClassName="current" to='/'><img src="rockifylogo.ico" width="50px" /></NavLink></li>
-      <li><NavLink activeClassName="current" to='/search'>Search</NavLink></li>
-      <li>Recommend</li>
-      <li>My Likes</li>
-      <div class = "login">
-        <li>Login</li>
+      <li><NavLink to='/'><img src="rockifylogo.ico" width="50px" /></NavLink></li>
+      <li><NavLink to='/search'>Search</NavLink></li>
+      <li><NavLink to='/recommend'>Recommend</NavLink></li>
+      <li><NavLink to='/mylikes'>My Likes</NavLink></li>
+      <div class = "logout">
+        <li><NavLink to='/'>Log Out</NavLink></li>
       </div>
     </div>
 );
 
-const Home = () => (
-  <div className='home'>
-    <h1> This is the homepage! </h1>
+const Login = () => (
+  <div className='login'>
+    <h1> Login </h1>
+    <label for="username">Username: </label>
+    <input type="text" />
+    <br></br>
+    <label for="password">Password: </label>
+    <input type="password" />
+    <br></br>
+    <button> Submit </button>
   </div>
 );
 
 const Search = () => (
-  <div class = "search">
-    <h1> The test is successful </h1>
+  <div className='search'>
+    <h1> Search for a track, artist, or album </h1>
+    <input type="text" placeholder="Search..."/>
+    <button> Search </button>
+    <br></br>
+    <input type="checkbox" id="Track" name="Track"/>
+    <label for="Track"> Track </label>
+    <input type="checkbox" id="Artist" name="Artist"/>
+    <label for="Artist"> Artist </label>
+    <input type="checkbox" id="Album" name="Album"/>
+    <label for="Album"> Album </label>
+  </div>
+);
+
+const Recommend = () => (
+  <div className='recommend'>
+    <h1> Recommend a track, artist, or album </h1>
+    <input type="text" placeholder="Recommend..."/>
+    <button> Recommend </button>
+    <br></br>
+    <input type="radio" id="TrackSearch" name="recommendSelection"/>
+    <label for="Track"> Track </label>
+    <input type="radio" id="Artist" name="recommendSelection"/>
+    <label for="Artist"> Artist </label>
+    <input type="radio" id="Album" name="recommendSelection"/>
+    <label for="Album"> Album </label>
+  </div>
+);
+
+const MyLikes = () => (
+  <div className='mylikes'>
+    <h1> My Likes </h1>
   </div>
 );
 
 const Main = () => (
   <Routes>
-    <Route path='/' element={<Home />} />
+    <Route path='/' element={<Login />} />
     <Route path='/search' element={<Search />} />
+    <Route path='/recommend' element={<Recommend />} />
+    <Route path='/mylikes' element={<MyLikes />} />
   </Routes>
 );
 
